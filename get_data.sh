73a393824ddd2c990e45469b7921c93042015956
manulera <manulera14@gmail.com>
2022-10-04 15:34:16 +0100

    fixed get_data

--- get_data.sh
+++ get_data.sh
@@ -9,10 +9,10 @@ gzip -d data/phenotype_annotations.phaf.gz
 cut -d $'\t' -f 2,4,9,10,11,12,18 data/phenotype_annotations.phaf|sort|uniq|grep -v $'\t'deletion|grep -v wild_type > data/alleles_pre_format.tsv
 python format_alleles.py data/alleles_pre_format.tsv > data/alleles.tsv
 
-# Get peptide sequences from PomBase
-curl -k https://www.pombase.org/data/genome_sequence_and_features/artemis_files/chromosome1.contig --output  data/chromosome1.contig
-curl -k https://www.pombase.org/data/genome_sequence_and_features/artemis_files/chromosome2.contig --output  data/chromosome2.contig
-curl -k https://www.pombase.org/data/genome_sequence_and_features/artemis_files/chromosome3.contig --output  data/chromosome3.contig
-curl -k https://www.pombase.org/data/genome_sequence_and_features/artemis_files/mating_type_region.contig --output  data/mating_type_region.contig
-curl -k https://www.pombase.org/data/genome_sequence_and_features/artemis_files/pMIT.contig --output  data/pMIT.contig
-curl -k https://www.pombase.org/data/genome_sequence_and_features/artemis_files/telomeric.contig --output  data/telomeric.contig
+# Get genome sequence with annotations
+curl -k https://www.pombase.org/releases/latest/pombe-embl/chromosome1.contig --output  data/chromosome1.contig
+curl -k https://www.pombase.org/releases/latest/pombe-embl/chromosome2.contig --output  data/chromosome2.contig
+curl -k https://www.pombase.org/releases/latest/pombe-embl/chromosome3.contig --output  data/chromosome3.contig
+curl -k https://www.pombase.org/releases/latest/pombe-embl/mating_type_region.contig --output  data/mating_type_region.contig
+curl -k https://www.pombase.org/releases/latest/pombe-embl/pMIT.contig --output  data/pMIT.contig
+curl -k https://www.pombase.org/releases/latest/pombe-embl/telomeric.contig --output  data/telomeric.contig
