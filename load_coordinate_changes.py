c4183bb757c3d531f7c00188b1e99fc7fe70f920
manulera <manulera14@gmail.com>
2022-10-14 18:07:25 +0100

    some coordinate changes

--- load_coordinate_changes.py
+++ load_coordinate_changes.py
@@ -1,13 +1,55 @@
 # %%
-from genome_functions import get_feature_location_from_string
+from genome_functions import gene_coords2genome_coords, get_feature_location_from_string
 from pandas import read_csv
-
+import warnings
+from Bio import BiopythonParserWarning
+import re
+import pickle
 
 data = read_csv('data/gene-coordinate-change-data.tsv', delimiter='\t', na_filter=False)
+
+
+with open('data/genome.pickle', 'rb') as ins:
+    contig_genome = pickle.load(ins)
+
+
+def fix_coordinate(coord: str, strand):
+    new_coord = coord.replace(" ", "")
+    if '-' in new_coord:
+        new_coord = new_coord.replace('-', '..')
+    if new_coord[-1] == ',':
+        new_coord = new_coord[:-1]
+    if ',' not in new_coord:
+        return new_coord
+
+    match = re.search(r'\(?(\d+\.\.\d+)((,)(\d+\.\.\d+))+\)?', new_coord)
+
+    new_coord = 'join(' + match.group().replace('(', '').replace(')', '') + ')'
+
+    if strand == -1:
+        new_coord = 'complement(' + new_coord + ')'
+
+    return new_coord
+
+
+def coord_needs_change(coord, strand):
+    with warnings.catch_warnings(record=True) as w:
+        try:
+            loc = get_feature_location_from_string(coord)
+
+        except AssertionError:
+            print(f'error in old location in line {i + 2}: {old_coord}')
+
+        w = list(filter(lambda i: issubclass(i.category, BiopythonParserWarning), w))
+        if len(w) or (strand == -1 and 'complement' not in coord):
+            return fix_coordinate(coord, strand)
+        return False
+
 
 for i, (systematic_id, old_coord, new_coord) in enumerate(zip(data['Systematic ID'], data['Old coordinates'], data['New coordinates'])):
     # Some of the lines have these fields empty
     if old_coord and new_coord:
+        print(systematic_id)
         try:
             old_loc = get_feature_location_from_string(old_coord)
         except AssertionError:
@@ -18,8 +60,12 @@ for i, (systematic_id, old_coord, new_coord) in enumerate(zip(data['Systematic I
         except AssertionError:
             print(f'error in new location in line {i + 2}: {new_coord}')
 
-        # new_loc = get_feature_location_from_string(new_coord)
+        _, strand = gene_coords2genome_coords(1, contig_genome[systematic_id])
 
-        # except AssertionError:
-        #     print(old_coord)
-        #     print(new_coord)
+        c_fix = coord_needs_change(new_coord, strand)
+        if c_fix:
+            print(i, 'new', new_coord, c_fix, sep='\t')
+
+        c_fix = coord_needs_change(new_coord, strand)
+        if c_fix:
+            print(i, 'old', old_coord, c_fix, sep='\t')
--- stats.py
+++ stats.py
@@ -3,6 +3,9 @@ import pandas
 
 data = pandas.read_csv('results/allele_results.tsv', sep='\t', na_filter=False)
 
+changed_coords = pandas.read_csv('data/gene-coordinate-change-data.tsv', sep='\t', na_filter=False)
+changed_coords_ids = set(changed_coords['Systematic ID'])
+
 aminoacid_data = data[~data['allele_type'].str.contains('nucl')]
 fixable_cols = (aminoacid_data['allele_type'] != 'other') & (aminoacid_data['allele_type'] != 'unknown')
 fixable_data = aminoacid_data[fixable_cols]
@@ -11,10 +14,15 @@ print('needs_fixing:', sum(fixable_data['needs_fixing']), sum(fixable_data['need
 
 autofix_cols = (fixable_data['rename_to'] != '') | (fixable_data['change_type_to'] != '')
 seqerror_cols = fixable_data['sequence_error'] != ''
-seqerror_fix_cols = fixable_data['sequence_error'].str.contains(', but')
+seqerror_fix_cols = fixable_data['sequence_error'].str.contains(', but') & ~fixable_data['systematic_id'].isin(changed_coords_ids)
+seqerror_change_coords = seqerror_cols & fixable_data['systematic_id'].isin(changed_coords_ids)
+seqerror_left = sum(seqerror_cols) - sum(seqerror_change_coords) - sum(seqerror_fix_cols)
+
 print('syntax_fixing:', sum(autofix_cols), sum(autofix_cols) / len(fixable_data))
 print('sequence_error:', sum(seqerror_cols), sum(seqerror_cols) / len(fixable_data))
-print('sequence_error_auto_fix:', sum(seqerror_fix_cols), sum(seqerror_fix_cols) / len(fixable_data))
+print('sequence_error_fix_coordinate_change:', sum(seqerror_fix_cols), sum(seqerror_fix_cols) / len(fixable_data))
+print('sequence_error_auto_fix:', sum(seqerror_change_coords), sum(seqerror_change_coords) / len(fixable_data))
+print('sequence_error_left:', seqerror_left, seqerror_left / len(fixable_data))
 
 print()
 nucleotide_data = data[data['allele_type'].str.contains('nucl')]
@@ -29,3 +37,9 @@ seqerror_fix_cols = fixable_data['sequence_error'].str.contains(', but')
 print('syntax_fixing:', sum(autofix_cols), sum(autofix_cols) / len(fixable_data))
 print('sequence_error:', sum(seqerror_cols), sum(seqerror_cols) / len(fixable_data))
 print('sequence_error_auto_fix:', sum(seqerror_fix_cols), sum(seqerror_fix_cols) / len(fixable_data))
+
+
+# seqerror_left_log = seqerror_cols & ~(seqerror_fix_cols | seqerror_change_coords)
+
+# for i in fixable_data['allele_description'][seqerror_left_log]:
+#     print(i)
