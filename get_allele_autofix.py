f7e6438395f1c66eee82081b1ac7f25d087cc3be
manulera <manulera14@gmail.com>
2022-10-27 16:34:32 +0100

    final changes to needs_supervision file

--- get_allele_autofix.py
+++ get_allele_autofix.py
@@ -49,31 +49,40 @@ output_data.loc[~seq_error & type_error & syntax_error, 'fix_type'] = 'type_erro
 if 'after_coords_rename_to' in output_data:
     fixed_after_coord_change = seq_error & (output_data['after_coords_sequence_error'] == '') & (output_data['after_coords_rename_to'] != '')
 
-    maybe_fix = ~seq_error & (output_data['after_coords_sequence_error'] == '') & (output_data['after_coords_rename_to'] != '')
-
     output_data.loc[fixed_after_coord_change, 'fix_type'] = 'update_coordinates'
     output_data.loc[fixed_after_coord_change, 'rename_to'] = output_data.loc[fixed_after_coord_change, 'after_coords_rename_to']
 
-    output_data.loc[maybe_fix, 'fix_type'] = 'maybe_fix'
-    output_data.loc[maybe_fix, 'rename_to'] = output_data.loc[maybe_fix, 'after_coords_rename_to']
+    # Partial aminoacid deletions that did not cause errors before or after the change in coordinates
+    coordinate_change_no_mutation = ~seq_error & (output_data['after_coords_sequence_error'] == '') & (output_data['after_coords_rename_to'] != '')
+    output_data.loc[coordinate_change_no_mutation, 'fix_type'] = 'coordinate_change_no_mutation'
 
 output_data = output_data.sort_values(['fix_type', 'systematic_id', 'allele_name'])
 
-auto_fix = output_data[output_data['fix_type'] != '']
+auto_fix = output_data[(output_data['fix_type'] != '') & (output_data['fix_type'] != 'coordinate_change_no_mutation')]
 auto_fix[['fix_type', 'systematic_id', 'allele_name', 'allele_type', 'allele_description', 'change_type_to', 'rename_to']].to_csv(args.output_auto_fix, sep='\t', index=False)
 
 
 if 'after_coords_rename_to' in output_data:
-    needing_supervision = seq_error & ~fixed_after_coord_change & (output_data['after_coords_sequence_error'].str.contains('>') | output_data['sequence_error'].str.contains('>')) & ~output_data['allele_type'].str.contains('nucl')
-    need_supervision_output = output_data[needing_supervision].copy()
-    need_supervision_output['proposed_fix'] = ''
-    need_supervision_output['after_coords_proposed_fix'] = ''
-    for i, row in need_supervision_output.iterrows():
+
+    # When coordinates are off by one
+    shift_coordinates_fix = seq_error & ~fixed_after_coord_change & (output_data['after_coords_sequence_error'].str.contains('>') | output_data['sequence_error'].str.contains('>')) & ~output_data['allele_type'].str.contains('nucl')
+    shift_coordinates_data = output_data[shift_coordinates_fix].copy()
+    shift_coordinates_data['proposed_fix'] = ''
+    shift_coordinates_data['after_coords_proposed_fix'] = ''
+    for i, row in shift_coordinates_data.iterrows():
         name = row['rename_to'] if row['rename_to'] else row['allele_description']
-        need_supervision_output.at[i, 'proposed_fix'] = seq_error_rename_to(name, row['sequence_error'])
+        shift_coordinates_data.at[i, 'proposed_fix'] = seq_error_rename_to(name, row['sequence_error'])
+        # When coordinates are odd by one and updated
         if row['after_coords_sequence_error']:
-            need_supervision_output.at[i, 'after_coords_proposed_fix'] = seq_error_rename_to(name, row['after_coords_sequence_error'])
+            shift_coordinates_data.at[i, 'after_coords_proposed_fix'] = seq_error_rename_to(name, row['after_coords_sequence_error'])
         pass
-    need_supervision_output['fix_type'] = 'shift_coordinates'
-    need_supervision_output.loc[output_data['after_coords_sequence_error'] != '', 'fix_type'] = 'update_and_shift_coordinates'
-    need_supervision_output[['fix_type', 'systematic_id', 'allele_name', 'allele_type', 'allele_description', 'change_type_to', 'rename_to', 'after_coords_rename_to', 'sequence_error', 'after_coords_sequence_error', 'proposed_fix', 'after_coords_proposed_fix']].to_csv(args.output_needs_supervision, sep='\t', index=False)
+    shift_coordinates_data['fix_type'] = 'shift_coordinates'
+    shift_coordinates_data.loc[output_data['after_coords_sequence_error'] != '', 'fix_type'] = 'update_and_shift_coordinates'
+
+    # Append the coordinate_change_no_mutation ones
+    coordinate_no_mutation_data = output_data[coordinate_change_no_mutation].copy()
+    coordinate_no_mutation_data['proposed_fix'] = coordinate_no_mutation_data['rename_to']
+    coordinate_no_mutation_data['after_coords_proposed_fix'] = coordinate_no_mutation_data['after_coords_rename_to']
+
+    needs_supervision_data = pandas.concat([coordinate_no_mutation_data, shift_coordinates_data])
+    needs_supervision_data[['fix_type', 'systematic_id', 'allele_name', 'allele_type', 'allele_description', 'change_type_to', 'rename_to', 'after_coords_rename_to', 'sequence_error', 'after_coords_sequence_error', 'proposed_fix', 'after_coords_proposed_fix']].to_csv(args.output_needs_supervision, sep='\t', index=False)
