e3b914d95f015a72313030d1943009a2b9787857
manulera <manulera14@gmail.com>
2022-10-27 18:12:31 +0100

    final one

--- get_allele_autofix.py
+++ get_allele_autofix.py
@@ -12,6 +12,8 @@ From the output file of 'fix_coordinates' or 'perform_qc' generate two output fi
 - --output_needs_supervision: the alleles that have sequence errors so may need human double-checking (this file is
     only created if the --input file has the after_coords columns)
 
+- --output_cannot_fix: the alleles that cannot be fixed with this pipeline.
+
 """
 import pandas
 import argparse
@@ -21,6 +23,7 @@ parser = argparse.ArgumentParser(description='Format alleles for auto-fix')
 parser.add_argument('--input', default='results/allele_results_after_coordinates.tsv')
 parser.add_argument('--output_auto_fix', default='results/allele_auto_fix.tsv')
 parser.add_argument('--output_needs_supervision', default='results/allele_needs_supervision.tsv')
+parser.add_argument('--output_cannot_fix', default='results/allele_cannot_fix.tsv')
 args = parser.parse_args()
 
 # Only the alleles that needed fixing
@@ -30,59 +33,72 @@ data = pandas.read_csv(args.input, delimiter='\t', na_filter=False)
 unknown_in_description = data['allele_description'] == 'unknown'
 data.loc[unknown_in_description, 'needs_fixing'] = True
 
-output_data = data[data['needs_fixing'] == True].copy()
+allele_error_data = data[data['needs_fixing'] == True].copy()
 
 # Create a new column to indicate the type of fix
-output_data['fix_type'] = ''
+allele_error_data['fix_type'] = ''
 
 # Set the value of fix_type for different types of errors
-seq_error = output_data['sequence_error'] != ''
-syntax_error = output_data['rename_to'] != ''
-type_error = output_data['change_type_to'] != ''
+seq_error = allele_error_data['sequence_error'] != ''
+syntax_error = allele_error_data['rename_to'] != ''
+type_error = allele_error_data['change_type_to'] != ''
 
-output_data.loc[~seq_error & syntax_error, 'fix_type'] = 'syntax_error'
-output_data.loc[~seq_error & (output_data['allele_description'] == 'unknown'), 'fix_type'] = 'unknown_in_description'
-output_data.loc[~seq_error & type_error, 'fix_type'] = 'type_error'
-output_data.loc[~seq_error & type_error & syntax_error, 'fix_type'] = 'type_error_and_syntax_error'
+allele_error_data.loc[~seq_error & syntax_error, 'fix_type'] = 'syntax_error'
+allele_error_data.loc[~seq_error & (allele_error_data['allele_description'] == 'unknown'), 'fix_type'] = 'unknown_in_description'
+allele_error_data.loc[~seq_error & type_error, 'fix_type'] = 'type_error'
+allele_error_data.loc[~seq_error & type_error & syntax_error, 'fix_type'] = 'type_error_and_syntax_error'
 
 # Same thing, including columns related to sequence coordinate fixing
-if 'after_coords_rename_to' in output_data:
-    fixed_after_coord_change = seq_error & (output_data['after_coords_sequence_error'] == '') & (output_data['after_coords_rename_to'] != '')
+if 'after_coords_rename_to' in allele_error_data:
+    fixed_after_coord_change = seq_error & (allele_error_data['after_coords_sequence_error'] == '') & (allele_error_data['after_coords_rename_to'] != '')
 
-    output_data.loc[fixed_after_coord_change, 'fix_type'] = 'update_coordinates'
-    output_data.loc[fixed_after_coord_change, 'rename_to'] = output_data.loc[fixed_after_coord_change, 'after_coords_rename_to']
+    allele_error_data.loc[fixed_after_coord_change, 'fix_type'] = 'update_coordinates'
+    allele_error_data.loc[fixed_after_coord_change, 'rename_to'] = allele_error_data.loc[fixed_after_coord_change, 'after_coords_rename_to']
 
     # Partial aminoacid deletions that did not cause errors before or after the change in coordinates
-    coordinate_change_no_mutation = ~seq_error & (output_data['after_coords_sequence_error'] == '') & (output_data['after_coords_rename_to'] != '')
-    output_data.loc[coordinate_change_no_mutation, 'fix_type'] = 'coordinate_change_no_mutation'
+    coordinate_change_no_mutation = ~seq_error & (allele_error_data['after_coords_sequence_error'] == '') & (allele_error_data['after_coords_rename_to'] != '')
+    allele_error_data.loc[coordinate_change_no_mutation, 'fix_type'] = 'coordinate_change_no_mutation'
 
-output_data = output_data.sort_values(['fix_type', 'systematic_id', 'allele_name'])
+    # Changing the reference sequence causes sequence errors and before there were no errors
+    coordinate_change_creates_error = ~seq_error & (allele_error_data['after_coords_sequence_error'] != '')
+    allele_error_data.loc[coordinate_change_creates_error, 'fix_type'] = 'coordinate_change_creates_error'
 
-auto_fix = output_data[(output_data['fix_type'] != '') & (output_data['fix_type'] != 'coordinate_change_no_mutation')]
-auto_fix[['fix_type', 'systematic_id', 'allele_name', 'allele_type', 'allele_description', 'change_type_to', 'rename_to']].to_csv(args.output_auto_fix, sep='\t', index=False)
+allele_error_data = allele_error_data.sort_values(['fix_type', 'systematic_id', 'allele_name'])
+auto_fix = (allele_error_data['fix_type'] != '') & ~coordinate_change_no_mutation & ~coordinate_change_creates_error
+auto_fix_data = allele_error_data[auto_fix]
+auto_fix_data[['fix_type', 'systematic_id', 'allele_name', 'allele_type', 'allele_description', 'change_type_to', 'rename_to']].to_csv(args.output_auto_fix, sep='\t', index=False)
+
+if 'after_coords_rename_to' not in allele_error_data:
+    allele_error_data[~auto_fix].to_csv(args.output_cannot_fix, sep='\t', index=False)
+    exit()
+
+# When coordinates are off by one
+shift_coordinates_fix = seq_error & ~fixed_after_coord_change & (allele_error_data['after_coords_sequence_error'].str.contains('>') | allele_error_data['sequence_error'].str.contains('>')) & ~allele_error_data['allele_type'].str.contains('nucl')
+shift_coordinates_data = allele_error_data[shift_coordinates_fix].copy()
+shift_coordinates_data['proposed_fix'] = ''
+shift_coordinates_data['after_coords_proposed_fix'] = ''
+for i, row in shift_coordinates_data.iterrows():
+    name = row['rename_to'] if row['rename_to'] else row['allele_description']
+    shift_coordinates_data.at[i, 'proposed_fix'] = seq_error_rename_to(name, row['sequence_error'])
+    # When coordinates are odd by one and updated
+    if row['after_coords_sequence_error']:
+        shift_coordinates_data.at[i, 'after_coords_proposed_fix'] = seq_error_rename_to(name, row['after_coords_sequence_error'])
+    pass
+shift_coordinates_data['fix_type'] = 'shift_coordinates'
+shift_coordinates_data.loc[allele_error_data['after_coords_sequence_error'] != '', 'fix_type'] = 'update_and_shift_coordinates'
+
+# When changing the reference sequence causes sequence errors and before there were no errors
+coordinate_change_creates_error_data = allele_error_data[coordinate_change_creates_error].copy()
+coordinate_change_creates_error_data['proposed_fix'] = ''
+coordinate_change_creates_error_data['after_coords_proposed_fix'] = ''
 
 
-if 'after_coords_rename_to' in output_data:
+# Append the coordinate_change_no_mutation ones
+coordinate_no_mutation_data = allele_error_data[coordinate_change_no_mutation].copy()
+coordinate_no_mutation_data['proposed_fix'] = ''
+coordinate_no_mutation_data['after_coords_proposed_fix'] = ''
 
-    # When coordinates are off by one
-    shift_coordinates_fix = seq_error & ~fixed_after_coord_change & (output_data['after_coords_sequence_error'].str.contains('>') | output_data['sequence_error'].str.contains('>')) & ~output_data['allele_type'].str.contains('nucl')
-    shift_coordinates_data = output_data[shift_coordinates_fix].copy()
-    shift_coordinates_data['proposed_fix'] = ''
-    shift_coordinates_data['after_coords_proposed_fix'] = ''
-    for i, row in shift_coordinates_data.iterrows():
-        name = row['rename_to'] if row['rename_to'] else row['allele_description']
-        shift_coordinates_data.at[i, 'proposed_fix'] = seq_error_rename_to(name, row['sequence_error'])
-        # When coordinates are odd by one and updated
-        if row['after_coords_sequence_error']:
-            shift_coordinates_data.at[i, 'after_coords_proposed_fix'] = seq_error_rename_to(name, row['after_coords_sequence_error'])
-        pass
-    shift_coordinates_data['fix_type'] = 'shift_coordinates'
-    shift_coordinates_data.loc[output_data['after_coords_sequence_error'] != '', 'fix_type'] = 'update_and_shift_coordinates'
+needs_supervision_data = pandas.concat([coordinate_no_mutation_data, coordinate_change_creates_error_data, shift_coordinates_data])
+needs_supervision_data[['fix_type', 'systematic_id', 'allele_name', 'allele_type', 'allele_description', 'change_type_to', 'rename_to', 'after_coords_rename_to', 'sequence_error', 'after_coords_sequence_error', 'proposed_fix', 'after_coords_proposed_fix']].to_csv(args.output_needs_supervision, sep='\t', index=False)
 
-    # Append the coordinate_change_no_mutation ones
-    coordinate_no_mutation_data = output_data[coordinate_change_no_mutation].copy()
-    coordinate_no_mutation_data['proposed_fix'] = coordinate_no_mutation_data['rename_to']
-    coordinate_no_mutation_data['after_coords_proposed_fix'] = coordinate_no_mutation_data['after_coords_rename_to']
-
-    needs_supervision_data = pandas.concat([coordinate_no_mutation_data, shift_coordinates_data])
-    needs_supervision_data[['fix_type', 'systematic_id', 'allele_name', 'allele_type', 'allele_description', 'change_type_to', 'rename_to', 'after_coords_rename_to', 'sequence_error', 'after_coords_sequence_error', 'proposed_fix', 'after_coords_proposed_fix']].to_csv(args.output_needs_supervision, sep='\t', index=False)
+allele_error_data[~auto_fix & ~shift_coordinates_fix & ~coordinate_change_no_mutation & ~coordinate_change_creates_error].to_csv(args.output_cannot_fix, sep='\t', index=False)
--- stats.py
+++ stats.py
@@ -1,45 +1,40 @@
-# %%
 import pandas
 
-data = pandas.read_csv('results/allele_results.tsv', sep='\t', na_filter=False)
-
-changed_coords = pandas.read_csv('data/gene-coordinate-change-data.tsv', sep='\t', na_filter=False)
-changed_coords_ids = set(changed_coords['Systematic ID'])
-
-aminoacid_data = data[~data['allele_type'].str.contains('nucl')]
-fixable_cols = (aminoacid_data['allele_type'] != 'other') & (aminoacid_data['allele_type'] != 'unknown')
-fixable_data = aminoacid_data[fixable_cols]
-print('fixable_aminoacid_alleles:', len(fixable_data), 1.)
-print('needs_fixing:', sum(fixable_data['needs_fixing']), sum(fixable_data['needs_fixing']) / len(fixable_data))
-
-autofix_cols = (fixable_data['rename_to'] != '') | (fixable_data['change_type_to'] != '')
-seqerror_cols = fixable_data['sequence_error'] != ''
-seqerror_fix_cols = fixable_data['sequence_error'].str.contains('>') & ~fixable_data['systematic_id'].isin(changed_coords_ids)
-seqerror_change_coords = seqerror_cols & fixable_data['systematic_id'].isin(changed_coords_ids)
-seqerror_left = sum(seqerror_cols) - sum(seqerror_change_coords) - sum(seqerror_fix_cols)
-
-print('syntax_fixing:', sum(autofix_cols), sum(autofix_cols) / len(fixable_data))
-print('sequence_error:', sum(seqerror_cols), sum(seqerror_cols) / len(fixable_data))
-print('sequence_error_fix_coordinate_change:', sum(seqerror_fix_cols), sum(seqerror_fix_cols) / len(fixable_data))
-print('sequence_error_auto_fix:', sum(seqerror_change_coords), sum(seqerror_change_coords) / len(fixable_data))
-print('sequence_error_left:', seqerror_left, seqerror_left / len(fixable_data))
+data = pandas.read_csv('results/allele_auto_fix.tsv', sep='\t', na_filter=False)
 
 print()
-nucleotide_data = data[data['allele_type'].str.contains('nucl')]
-fixable_cols = (nucleotide_data['allele_type'] != 'other') & (nucleotide_data['allele_type'] != 'unknown')
-fixable_data = nucleotide_data[fixable_cols]
-print('fixable_nucleotide_alleles:', len(fixable_data), 1.)
-print('needs_fixing:', sum(fixable_data['needs_fixing']), sum(fixable_data['needs_fixing']) / len(fixable_data))
+print('\033[0;32m', '> auto-fix alleles: ', '\033[0m', len(data), sep='')
 
-autofix_cols = (fixable_data['rename_to'] != '') | (fixable_data['change_type_to'] != '')
-seqerror_cols = fixable_data['sequence_error'] != ''
-seqerror_fix_cols = fixable_data['sequence_error'].str.contains('>')
-print('syntax_fixing:', sum(autofix_cols), sum(autofix_cols) / len(fixable_data))
-print('sequence_error:', sum(seqerror_cols), sum(seqerror_cols) / len(fixable_data))
-print('sequence_error_auto_fix:', sum(seqerror_fix_cols), sum(seqerror_fix_cols) / len(fixable_data))
+for f in pandas.unique(data.fix_type):
+    print('  ', f, sum(data.fix_type == f))
 
+data = pandas.read_csv('results/allele_needs_supervision.tsv', sep='\t', na_filter=False)
 
-# seqerror_left_log = seqerror_cols & ~(seqerror_fix_cols | seqerror_change_coords)
+print()
+print('\033[0;32m', '> need_supervision: ', '\033[0m', len(data), sep='')
 
-# for i in fixable_data['allele_description'][seqerror_left_log]:
-#     print(i)
+for f in pandas.unique(data.fix_type):
+    print('  ', f, sum(data.fix_type == f))
+
+data = pandas.read_csv('results/allele_cannot_fix.tsv', sep='\t', na_filter=False)
+
+print()
+
+nt_number = sum(data['allele_type'].str.contains('nucl'))
+aa_number = sum(data['allele_type'].str.contains('amino'))
+other_number = sum(~data['allele_type'].str.contains('amino') & ~data['allele_type'].str.contains('nucl'))
+
+invalid_error = sum(data['invalid_error'] != '')
+sequence_error = sum(data['sequence_error'] != '')
+pattern_error = sum(data['pattern_error'] != '')
+
+print('\033[0;32m', '> cannot fix alleles: ', '\033[0m', len(data), sep='')
+print('   nucleotide alleles:', nt_number)
+print('   aminoacid alleles:', aa_number)
+print('   disruption alleles:', other_number)
+print()
+print('   sequence_error:', sequence_error, 'of which', sum((data['sequence_error'] != '') & data['allele_type'].str.contains('nucl')), 'nucleotide')
+print('   pattern_error:', pattern_error)
+print('   invalid_error:', invalid_error)
+
+print()
