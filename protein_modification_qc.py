223a86e028162c7dd5b60e3f357bcbc58a2922fb
Manuel Lera Ramirez <manulera14@gmail.com>
2022-12-14 10:46:38 +0100

    better errors and aggregation

--- protein_modification_qc.py
+++ protein_modification_qc.py
@@ -1,7 +1,7 @@
 import pandas
 from grammar import check_sequence_single_pos, aa
+from refinement_functions import replace_allele_features, sort_result
 import pickle
-import re
 
 with open('data/genome.pickle', 'rb') as ins:
     genome = pickle.load(ins)
@@ -15,24 +15,43 @@ data = data[data['sequence_position'] != '']
 
 def check_func(row):
     if row['systematic_id'] not in genome:
-        return 'systematic_id not in genome'
+        return 'systematic_id not in genome', ''
 
     gene = genome[row['systematic_id']]
 
     if 'CDS' not in gene:
-        return 'gene has no CDS'
+        return 'missing_CDS', ''
 
-    match_obj = re.match(f'({aa})(\d+)', row['sequence_position'])
+    result = replace_allele_features([f'(?<!{aa})({aa})(\d+){aa}?'], [row['sequence_position']], [])
 
-    if not match_obj:
-        return 'pattern error'
+    # Extract the matched and unmatched elements
+    matches, unmatched = sort_result(result)
 
-    return check_sequence_single_pos(match_obj.groups(), gene, 'peptide')
+    if len(unmatched):
+        return 'pattern_error', ''
+
+    correct_name = ','.join(''.join(match.groups()) for match in matches)
+
+    change_sequence_position_to = ''
+    if correct_name != row['sequence_position']:
+        change_sequence_position_to = correct_name
+
+    return '|'.join([check_sequence_single_pos(match.groups(), gene, 'peptide') for match in matches]), change_sequence_position_to
 
 
-data['sequence_error'] = data.apply(check_func, axis=1)
+extra_cols = data.apply(check_func, axis=1, result_type='expand')
+data.loc[:, 'sequence_error'] = extra_cols.loc[:, 0]
+data.loc[:, 'change_sequence_position_to'] = extra_cols.loc[:, 1]
+# data.loc[:, ['sequence_error', 'change_sequence_position_to']] = data.apply(check_func, axis=1, result_type='expand')
+data.sort_values(['systematic_id', 'sequence_position'], inplace=True)
 data.to_csv('results/protein_modification_results.tsv', sep='\t', index=False)
 
-data[data['sequence_error'] != ''].to_csv('results/protein_modification_results_errors.tsv', sep='\t', index=False)
+error_data = data[(data['sequence_error'] != '') | (data['change_sequence_position_to'] != '')].copy()
+error_data.to_csv('results/protein_modification_results_errors.tsv', sep='\t', index=False)
 
 # Aggregate the errors
+sequence_error_data = error_data[~error_data['sequence_error'].isin(['', 'pattern_error', 'missing_CDS'])].copy()
+sequence_error_data.loc[sequence_error_data['change_sequence_position_to'] != '', 'sequence_position'] = sequence_error_data['change_sequence_position_to']
+
+aggregated_sequence_error_data = sequence_error_data[['systematic_id', 'reference', 'sequence_position', 'sequence_error']].drop_duplicates().groupby(['systematic_id', 'reference'], as_index=False).agg({'sequence_position': '|'.join, 'sequence_error': '|'.join})
+aggregated_sequence_error_data.to_csv('results/protein_modification_results_errors_aggregated.tsv', sep='\t', index=False)
