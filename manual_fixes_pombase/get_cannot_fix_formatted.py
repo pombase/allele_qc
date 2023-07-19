19e8422fa5e6bdadf17e1f9345e6d2fe0b2bb06b
Manuel Lera Ramirez <manulera14@gmail.com>
2023-07-19 17:19:54 +0200

    fix gde1 alleles

--- manual_fixes_pombase/get_cannot_fix_formatted.py
+++ manual_fixes_pombase/get_cannot_fix_formatted.py
@@ -3,7 +3,11 @@ import pandas
 all_alleles = pandas.read_csv('../data/alleles.tsv', sep='\t', na_filter=False)
 data = pandas.read_csv('../results/allele_cannot_fix_sequence_errors.tsv', sep='\t', na_filter=False)
 
-already_fixed = pandas.read_csv('manual_cannot_fix_new.tsv', sep='\t', na_filter=False)
+already_fixed = pandas.concat([
+    pandas.read_csv('manual_cannot_fix_new.tsv', sep='\t', na_filter=False),
+    pandas.read_csv('allele_manual_changes_formatted.tsv', sep='\t', na_filter=False),
+    pandas.read_csv('cannot_find.tsv', sep='\t', na_filter=False),
+    ])
 
 data = data[~data['allele_name'].isin(already_fixed['allele_name'])].copy()
 
