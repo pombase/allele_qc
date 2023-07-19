e64753266d3f66b2878af22ac1d5f84fc934d3dc
Manuel Lera Ramirez <manulera14@gmail.com>
2023-07-19 17:07:32 +0200

    filled in missing change_name_to

--- manual_fixes_pombase/fill_change_name_for_description_in_name.py
+++ manual_fixes_pombase/fill_change_name_for_description_in_name.py
@@ -2,6 +2,8 @@ import pandas
 import sys
 
 manual_changes = pandas.read_csv(sys.argv[1], sep="\t", na_filter=False)
+original_dataset = manual_changes.copy()
+column_order = list(original_dataset.columns)
 manual_changes.fillna('', inplace=True)
 
 # only the ones that did not have a value in change_name_to
@@ -19,4 +21,9 @@ def get_change_name_to(row):
 
 manual_changes['change_name_to'] = manual_changes.apply(get_change_name_to, axis=1)
 manual_changes = manual_changes[manual_changes['change_name_to'] != ''].copy()
-manual_changes.to_csv('names_that_should_be_fixed.tsv', sep="\t", index=False)
+print(original_dataset.shape)
+final_dataset = original_dataset.merge(manual_changes[['allele_name', 'change_name_to']], on='allele_name', how='left')
+final_dataset.fillna('', inplace=True)
+final_dataset['change_name_to'] = final_dataset.apply(lambda row: row['change_name_to_y'] if row['change_name_to_y'] != '' else row['change_name_to_x'], axis=1)
+final_dataset.drop(['change_name_to_x', 'change_name_to_y'], axis=1, inplace=True)
+final_dataset[column_order].to_csv('names_filled.tsv', sep="\t", index=False)
