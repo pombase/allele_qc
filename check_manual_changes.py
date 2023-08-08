3218f934f4530f64c703b7024aa6851b4b4014fc
Manuel Lera Ramirez <manulera14@gmail.com>
2023-08-08 17:18:12 +0200

    misc fixes associated with new grammar usage

--- check_manual_changes.py
+++ check_manual_changes.py
@@ -1,7 +1,7 @@
 import sys
 import pandas
 from refinement_functions import check_allele_description
-from grammar import allowed_types, aminoacid_grammar_old, nucleotide_grammar_old
+from grammar import allowed_types_dict, aminoacid_grammar, nucleotide_grammar
 from models import SyntaxRule
 import pickle
 
@@ -9,17 +9,17 @@ import pickle
 def main(input_file):
     with open('data/genome.pickle', 'rb') as ins:
         genome = pickle.load(ins)
-    syntax_rules_aminoacids = [SyntaxRule.parse_obj(r) for r in aminoacid_grammar_old]
-    syntax_rules_nucleotides = [SyntaxRule.parse_obj(r) for r in nucleotide_grammar_old]
+    syntax_rules_aminoacids = [SyntaxRule.parse_obj(r) for r in aminoacid_grammar]
+    syntax_rules_nucleotides = [SyntaxRule.parse_obj(r) for r in nucleotide_grammar]
     data = pandas.read_csv(input_file, sep='\t')
     data.fillna('', inplace=True)
     for i, line in data.iterrows():
         allele_type = line['change_type_to'] if line['change_type_to'] != '' else line['allele_type']
         allele_description = line['change_description_to'] if line['change_description_to'] != '' else line['allele_description']
         if 'amino' in allele_type:
-            check = check_allele_description(allele_description, syntax_rules_aminoacids, allele_type, allowed_types, genome[line['systematic_id']])
+            check = check_allele_description(allele_description, syntax_rules_aminoacids, allele_type, allowed_types_dict, genome[line['systematic_id']])
         else:
-            check = check_allele_description(allele_description, syntax_rules_nucleotides, allele_type, allowed_types, genome[line['systematic_id']])
+            check = check_allele_description(allele_description, syntax_rules_nucleotides, allele_type, allowed_types_dict, genome[line['systematic_id']])
         if check['needs_fixing']:
             print('\t'.join(line.tolist()))
             print('seq_error:', check['sequence_error'], 'change_description_to:', check['change_description_to'], 'change_type_to:', check['change_type_to'])
