b8571c4b62915ccc150d4fc2762bcfef048e215b
Manuel Lera Ramirez <manulera14@gmail.com>
2023-08-10 19:41:43 +0200

    Fixes for pipeline (#95)
    * use current grammar in allele_qc
    
    * less permissive separator for allele parts
    
    * loading bar to follow progress of load_genome
    
    * include transvar scripts in the pipeline and run again

--- run_analysis.sh
+++ run_analysis.sh
@@ -3,5 +3,7 @@ bash get_data.sh
 python build_alignment_dict.py
 python protein_modification_qc.py
 python protein_modification_auto_fix.py
+python protein_modification_transvar.py
 python allele_qc.py
 python allele_auto_fix.py
+python allele_transvar.py
