4073d2c92e910cc80618a6dc0b0c282280a9d3ce
Manuel Lera-Ramirez <manulera14@gmail.com>
2023-08-17 16:53:11 +0200

    Samtools from apt (#100)
    * Install samtools/htslib from APT (#99)
    
    * Install samtools/htslib from APT
    
    ... instead of compiling
    
    * Install samtools/htslib from APT for workflows
    
    * Use sudo when installing with APT
    
    * some extra changes and better doc
    
    ---------
    
    Co-authored-by: Kim Rutherford <kmr@aska.gen.nz>

--- set_up_transvar.sh
+++ set_up_transvar.sh
@@ -4,15 +4,12 @@ set -e
 transvar config -k reference -v "data/pombe_genome.fa" --refversion pombe_genome
 transvar index --ensembl data/pombe_genome.gtf --reference data/pombe_genome.fa
 
-# At this point, we could delete the lib folder, but it's very small
+# Hacky way to use the functions inside another script
+cp $(which transvar) ./transvar_main_script.py
 
 # See whether it works
 transvar panno -i 'SPAC3F10.09:p.E2A' --ensembl data/pombe_genome.gtf.transvardb --reference data/pombe_genome.fa
 transvar ganno -i 'I:g.2832796A>T' --ensembl data/pombe_genome.gtf.transvardb --reference data/pombe_genome.fa
 transvar ganno -i 'I:g.2832795T>A' --ensembl data/pombe_genome.gtf.transvardb --reference data/pombe_genome.fa
-
-# Hacky way to use the functions inside another script
-cp $(which transvar) ./transvar_main_script.py
-
 transvar panno -i 'SPBC1198.04c:p.T566S' --ensembl data/pombe_genome.gtf.transvardb --reference data/pombe_genome.fa
 transvar panno -i 'SPAPB1A10.09:p.S372_N374delinsAAA' --ensembl data/pombe_genome.gtf.transvardb --reference data/pombe_genome.fa  --gseq
